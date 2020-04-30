cf709778fc7004dc2e1713649033168952b5d39c
alexanderjaramillo <alexunjm@gmail.com>
2020-04-30 01:46:20 -0500

    transform node to browser project

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var tsify = require('tsify');
+var paths = {
+  pages: ['src/*.html']
+};
 
-gulp.task("default", function () {
-  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+gulp.task('copy-html', function () {
+  return gulp.src(paths.pages)
+    .pipe(gulp.dest('dist'));
 });
+
+gulp.task('default', gulp.series(gulp.parallel('copy-html'), function () {
+  return browserify({
+    basedir: '.',
+    debug: true, // let me debug original src ts files
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
+}));
